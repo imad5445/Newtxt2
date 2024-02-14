3bafbafe29db3d7bf0688228814b1561b61abc5c
imad5445 <157965306+imad5445@users.noreply.github.com>
2024-02-15 03:38:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6843556382:AAFgQGtaErBt5ry1zc1LqRp-nnIHIktDnao")
+    API_ID = int(os.environ["API_ID", 27060147]
+    API_HASH = os.environ["API_HASH", "70f93c98c61a40f69c0f830bd4fece7c"]
+    AUTH_USERS = "6607659765 """
+
